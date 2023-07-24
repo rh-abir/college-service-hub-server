365be493e5b0e1862f33b7bb48b049b0973c7171
rh-abir <rhabir.co@gmail.com>
2023-07-24 20:49:19 +0600

    update to vercial

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
@@ -14,7 +15,7 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://collageAdmin:9jn4pvwj6WkwaVd6@cluster0.jvfq5i3.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jvfq5i3.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
